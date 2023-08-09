471befb04af041f1ccbf81cafbfc5f2c7666bade
Horleryheancarh <oladipoolayinka7@gmail.com>
2023-08-09 12:29:01 +0100

    init and finished home page

---
+++ client/src/pages/Customizer.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react'
+import from 'framer-motion'
+
+const Customizer = () => {
+  return (
+    <div>Customizer</div>
+  )
+}
+
+export default Customizer
